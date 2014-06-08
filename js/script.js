3b8ffb8640bc862c5212a019b55923fe4670a8a5
Devin Collins <devin@imdevinc.com>
2014-06-08 16:32:17 -0500

    Updated JS for looper

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
 		type: 'GET',
 		url: 'functions.php'
 	}).done(function(data) {
-		$('#looper .looper-inner').html(data);
+		$('#looper .looper-inner').append(data);
 		$('time').humaneDates();
-		$('.looper').looper();
+		$('#looper .looper-inner').find('.item').first().remove();
 	}).error(function(data) {
 		$('#looper .looper-inner').html('<div class="item"><div class="alert alert-danger">There was an issue loading Github repos for ImDevinC.  Please try again later or visit <a href="http://www.github.com/ImDevinC" target="_blank">Github</a>.</div></div>');
 	});
