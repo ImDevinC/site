b43690f9f9be24d60ec69e17a86d4dcdce88da05
Devin Collins <me@imdevinc.com>
2022-10-10 11:08:54 -0700

    Add alternative names

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,6 +1,9 @@
 resource "aws_acm_certificate" "public" {
   domain_name = "imdevinc.com"
   validation_method = "DNS"
+  subject_alternative_names = [
+    "*.imdevinc.com"
+  ]
   lifecycle {
     create_before_destroy = true
   }
