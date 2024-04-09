7cd2d54040778db11862ea2808d1da07f77d312c
Devin Collins <me@imdevinc.com>
2024-04-09 10:06:04 -0700

    remove mollycollins.me

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -9,12 +9,3 @@ resource "aws_acm_certificate" "public" {
   }
   provider = aws.useast1
 }
-
-resource "aws_acm_certificate" "molly" {
-  domain_name       = "mollycollins.me"
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-  provider = aws.useast1
-}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,8 +1,7 @@
 module "zones" {
   source = "terraform-aws-modules/route53/aws//modules/zones"
   zones = {
-    (local.domain_name)  = {}
-    (local.molly_domain) = {}
+    (local.domain_name) = {}
   }
 }
 
@@ -14,18 +13,11 @@ locals {
       type    = dvo.resource_record_type
     }
   ]
-  molly_records = [
-    for dvo in aws_acm_certificate.molly.domain_validation_options : {
-      name    = dvo.resource_record_name
-      records = [dvo.resource_record_value]
-      type    = dvo.resource_record_type
-    }
-  ]
 }
 
 module "records" {
-  source    = "terraform-aws-modules/route53/aws//modules/records"
-  zone_name = local.domain_name
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  zone_id = module.zones.route53_zone_zone_id[local.domain_name]
 
   records = [
     {
@@ -61,46 +53,6 @@ module "records" {
       name    = ""
       type    = "TXT"
       records = [local.keybase_validation]
-      ttl     = 3600
-    },
-    {
-      name = ""
-      type = "MX",
-      records = [
-        "1 aspmx.l.google.com",
-        "5 alt1.aspmx.l.google.com",
-        "5 alt2.aspmx.l.google.com",
-        "10 alt3.aspmx.l.google.com",
-        "10 alt4.aspmx.l.google.com"
-      ]
-      ttl = 1800
-    }
-  ]
-
-  depends_on = [module.zones]
-}
-
-module "records_molly" {
-  source    = "terraform-aws-modules/route53/aws//modules/records"
-  zone_name = local.molly_domain
-  records = [
-    {
-      name    = "www"
-      type    = "CNAME"
-      records = [local.molly_domain]
-      ttl     = 3600
-    },
-    {
-      name               = local.molly_records[0].name
-      records            = local.molly_records[0].records
-      type               = local.molly_records[0].type
-      full_name_override = true
-      ttl                = 300
-    },
-    {
-      name    = "google-site-verification"
-      type    = "TXT"
-      records = [local.molly_site_verification]
       ttl     = 3600
     },
     {
