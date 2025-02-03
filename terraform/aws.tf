a29d11ddb5b01888e1272027c0c021a67b13f8ee
Devin Collins <me@imdevinc.com>
2025-02-03 08:40:51 -0800

    Disabling AWS for now

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,8 +1,8 @@
-module "aws" {
-  source        = "./aws"
-  vpc_cidr      = "10.0.0.0/16"
-  ami_id        = "ami-07a8fb43374538166"
-  instance_type = "t4g.small"
-  subnets       = ["us-west-1b", "us-west-1c"]
-  region        = "us-west-1"
-}
+//module "aws" {
+//  source        = "./aws"
+//  vpc_cidr      = "10.0.0.0/16"
+//  ami_id        = "ami-07a8fb43374538166"
+//  instance_type = "t4g.small"
+//  subnets       = ["us-west-1b", "us-west-1c"]
+//  region        = "us-west-1"
+//}
